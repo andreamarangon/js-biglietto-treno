c9ce836ba0a09818cb929ba5b10b34d68aa0edf6
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-01 15:56:46 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,17 +11,17 @@ var intero = chilometri * 0.21;
 console.log(intero);
 
 //calcolo il prezzo del biglietto ridotto
-var ridotto = intero - (intero * 20 / 100)
+var ridotto = (intero - (intero * 20 / 100)).toFixed(2);
 console.log(ridotto);
 
 //calcolo il prezzo del biglietto anziani
-var anziani = intero - (intero * 40 / 100)
+var anziani = (intero - (intero * 40 / 100)).toFixed(2);
 console.log(anziani);
 
 if ((eta >= 18) && (eta <= 65)) {
-  document.getElementById('prezzo').innerHTML = "il prezzo del biglietto è: " + intero + '€';
+  document.getElementById('prezzo').innerHTML = "L'importo da pagare è di: " + intero + '€';
 } else if (eta < 18) {
-  document.getElementById('prezzo').innerHTML = "il prezzo del biglietto è: " + ridotto + '€';
+  document.getElementById('prezzo').innerHTML = "L'importo da pagare è di: " + ridotto + '€';
 } else {
-  document.getElementById('prezzo').innerHTML = "il prezzo del biglietto è: " + anziani + '€';
+  document.getElementById('prezzo').innerHTML = "L'importo da pagare è di: " + anziani + '€';
 }
